d751f9b50bd722c327b52cd68e857e926beea7c2
fiorellachilcon <fiorellachilc_20@hotmail.com>
2020-08-26 11:24:09 -0500

    working with data, ember store

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,28 +1,36 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-const COMMUNITY_CATEGORIES = [
-  'Condo',
-  'Townhouse',
-  'Apartment'
-];
 export default class IndexRoute extends Route {
+  @service store;
+  
   async model() {
-    let response = await fetch('/api/rentals.json');
-    // let parsed = await response.json();
-    // return parsed;
-     let { data } = await response.json();
-
-    return data.map(model => {
-      let { attributes, id } = model;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    });
+    return this.store.findAll('rental');
   }
 }
+// const COMMUNITY_CATEGORIES = [
+//   'Condo',
+//   'Townhouse',
+//   'Apartment'
+// ];
+// export default class IndexRoute extends Route {
+//   async model() {
+//     let response = await fetch('/api/rentals.json');
+//     // let parsed = await response.json();
+//     // return parsed;
+//      let { data } = await response.json();
+
+//     return data.map(model => {
+//       let { attributes, id } = model;
+//       let type;
+
+//       if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+//         type = 'Community';
+//       } else {
+//         type = 'Standalone';
+//       }
+
+//       return { id, type, ...attributes };
+//     });
+//   }
+// }
